d8758bb79d20aa79e281c28830005a3fb83fbd01
ldandoy <ldandoy@overconsulting.net>
2022-06-08 10:42:10 +0200

    Add nodejs

--- node_1/routes/todo.js
+++ node_1/routes/todo.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const Router = express.Router()
 
@@ -13,21 +14,48 @@ Todo: {
 */
 
 Router.get('/', (request, response) => {
-    response.status(200).json(todos)
+    return response.status(200).json(todos)
 })
 
 Router.post('/', (req, res) => {
     const {todo} = req.body
 
+    todo.id = uuidv4()
+
     todos.push(todo)
 
-    res.status(200).json(todo)
+    return res.status(200).json(todo)
 })
 
 Router.put('/:index', (req, res) => {
+    const {index} = req.params // from the route
+    // const {test} = req.query // For params from url ?test=toto
+    
+    const {isDone, label} = req.body // from the body
+    
+    if (typeof todos[index] != 'undefined') {
+        if (typeof isDone != 'undefined') {
+            todos[index].isDone = isDone
+        }
+        if (typeof label != 'undefined') {
+            todos[index].label = label
+        }
+    } else {
+        return res.status(500).json({"msg": "Todo not found !"})
+    }
+
+    return res.status(200).json(todos[index])
+})
+
+Router.delete('/:index', (req, res) => {
     const {index} = req.params
-    const {test} = req.query
-    console.log(index, test)
+
+    if (typeof todos[index] != 'undefined') {
+        todos.splice(index, 1)
+        return res.status(200).json({"msg": "Todo well deleted !"})
+    } else {
+        return res.status(500).json({"msg": "Todo not found !"})
+    }
 })
 
 module.exports = Router
