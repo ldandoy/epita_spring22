6dc634390ccc0e8b6c55d73fddfea27c16a0f2a4
ldandoy <ldandoy@overconsulting.net>
2022-06-15 11:22:29 +0200

    Add env and session

--- node_1/index.js
+++ node_1/index.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const session = require('express-session')
 
 try {
-    mongoose.connect('mongodb://127.0.0.1:27018/epita', {
+    mongoose.connect(process.env.DB_URL, {
         authSource: "admin",
-        user: "root",
-        pass: "example",
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASSWORD,
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
@@ -20,6 +22,17 @@ const messageRouter = require('./routes/messageRoute')
 const authRouter = require('./routes/authRoute')
 
 const app = express()
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        httpOnly: true
+    }
+}))
+
 app.use(morgan('dev'))
 app.use(express.json())
 
--- node_1/routes/authRoute.js
+++ node_1/routes/authRoute.js
@@ -20,12 +20,12 @@ Router.post('/register', async(req, res) => {
             })
 
             const user = await User.save()
-            res.status(200).json(user)
+            return res.status(200).json(user)
         } catch (error) {
-            res.status(500).json({'msg': error.message})
+            return res.status(500).json({'msg': error.message})
         }
     } else {
-        res.status(500).json({"msg": "Email and password are required, and password should be at least 6 caracters !"})
+        return res.status(500).json({"msg": "Email and password are required, and password should be at least 6 caracters !"})
     }
 })
 
@@ -39,11 +39,46 @@ Router.post('/login', async (req, res) => {
     const {email, password} = req.body
 
     if ( (email && email !== "") && (password && password !== "")) {
-        const user = await userModel.findOne({'email': email})
-        console.log(user)
+        try {
+            const user = await userModel.findOne({'email': email})
+
+            if (!user) {
+                return res.status(500).json({'msg': 'User not found !'})
+            }
+
+            if (!user.active) {
+                return res.status(500).json({'msg': 'User not active !'})
+            }
+        
+            if (!bcrypt.compareSync(password, user.password)) {
+                return res.status(500).json({'msg': "Email or Password don't match !"})
+            }
+
+            req.session.user = {
+                "_id": user._id,
+                "username": user.username,
+                "email": user.email,
+                "active": user.active,
+                "created_at": user.created_at,
+                "updated_at": user.updated_at,
+                "__v": user.__v
+            }
+
+            return res.status(200).json({'msg': 'User identified !'})
+        } catch (error) {
+            return res.status(500).json({'msg': error.message})
+        }
     } else {
-        res.status(500).json({"msg": "Email and password are required !"})
+        return res.status(500).json({"msg": "Email and password are required !"})
     }
+})
+
+Router.get('/me', (req, res) => {
+    if (req.session.user) {
+        return res.status(200).json(req.session.user)
+    }
+
+    return res.status(500).json({"msg": "You are not authenticated !"})
 })
 
 module.exports = Router
